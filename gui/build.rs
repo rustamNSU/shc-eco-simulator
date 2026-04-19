b0ed511f35f71200d4a3fb921b7bbfa0796389bb
rustam <afaritovich@mail.ru>
2026-04-20 00:27:40 +0700

    added icon

--- gui/build.rs
+++ gui/build.rs
@@ -1,3 +1,9 @@
 fn main() {
     slint_build::compile("ui/main.slint").expect("failed to compile Slint UI");
+
+    #[cfg(target_os = "windows")]
+    winresource::WindowsResource::new()
+        .set_icon("assets/concept06_tax.ico")
+        .compile()
+        .expect("failed to embed Windows executable icon");
 }
